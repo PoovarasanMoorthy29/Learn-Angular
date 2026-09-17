bb6c7293dcd6a4a73f3a2e4a2ff3fec5fa97ab36
PoovarasanMoorthy29 <poovarasanmoorthy375@gmail.com>
2026-09-17 13:05:37 +0530

    FEAT: @Input decarator used to send data from parent to child

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,20 +1,19 @@
 import { Component, Input, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SideBar } from '../shared/side-bar/side-bar';
-import {User} from '../models/User';
+import { User } from '../models/User';
 import { Data } from '../shared/data/data';
 
 @Component({
-  imports: [RouterOutlet,SideBar,Data],
+  imports: [RouterOutlet, SideBar, Data],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
-  
 })
 export class App {
-  user:User={
-    name:'John',
-    age:23
+  user: User = {
+    name: 'John',
+    age: 23,
   };
 
 }
